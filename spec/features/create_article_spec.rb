0cb7f4f9a07f1b8d79e36f7ea4ff305392e8789f
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-10-04 13:19:19 -0300

    Add integration test for article position

--- spec/features/create_article_spec.rb
+++ spec/features/create_article_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
-describe "Create a article with success" do
+describe "Create a article with success", :type => :feature do
+    before :each do
+      Article.create([description: "article one", description: "article two"])
+    end
   scenario "When submit to a new article" do
     visit root_path
     within("#new_article") do
@@ -7,5 +10,10 @@ describe "Create a article with success" do
     end
     click_on("Submit Query")
     expect(page).to have_content 'Some text'
+  end
+  scenario "When articles are already create, needs position" do
+    visit root_path
+    expect(page).to_not have_content '0.'
+    expect(page).to have_content '1.'
   end
 end
