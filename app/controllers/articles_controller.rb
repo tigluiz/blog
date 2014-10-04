b05763bcd5d371261f7c911ef5eb33ea9466826c
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-10-04 11:21:03 -0300

    Prepare controller to works on a single page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,7 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
-
   def index
-    @articles = Article.all
-  end
-
-  def show
-  end
-
-  def new
     @article = Article.new
-  end
-
-  def edit
+    @articles = Article.all
   end
 
   def create
@@ -20,8 +9,8 @@ class ArticlesController < ApplicationController
 
     respond_to do |format|
       if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
+        format.html { redirect_to :back, notice: 'Article was successfully created.' }
+        format.json { render :back, status: :created, location: @article }
       else
         format.html { render :new }
         format.json { render json: @article.errors, status: :unprocessable_entity }
@@ -29,32 +18,9 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
     def article_params
-      params.require(:article).permit(:name)
+      params.require(:article).permit(:description)
     end
 end
