422b32ac560ffc16ffeec9fd6cc0ec9d3300ae13
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-10-05 20:13:16 -0300

    Remove binding pry

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,6 @@
 class CommentsController < ApplicationController
   def create
     @article = Article.find_by_id(params[:article_id])
-    binding.pry
     if params[:comment][:master_comment_id]
       @comment = Comment.find_by_id(params[:comment][:master_comment_id])
       @comment.child_comments.create(comments_params.merge(article_id: @article.id))
